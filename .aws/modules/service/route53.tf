ded17ffa46da1a35ee25d4a143fa4a95252ae25e
Jose A Gonzalez <jagonzalez@rbi.com>
2022-05-15 12:14:54 +0100

    Try adding apiId in ssm to create domain and api mapping

--- .aws/modules/service/route53.tf
+++ .aws/modules/service/route53.tf
@@ -1,9 +1,14 @@
-data "aws_ssm_parameter" "ami" {
+data "aws_ssm_parameter" "cloud_dist" {
+  count = var.create_domain ? 1 : 0 # only create at the later stage of the pipeline
   name = "/devportal/${var.short_region}/${var.stage}/${var.brand}/cloudfrontdist"
 }
 
-data "aws_acm_certificate" "acm_cert" {
+data "aws_ssm_parameter" "api_id" {
   count = var.create_domain ? 1 : 0 # only create at the later stage of the pipeline
+  name = "/devportal/${var.short_region}/${var.stage}/${var.brand}/apiId"
+}
+
+data "aws_acm_certificate" "acm_cert" {
   provider = aws.route53
 
   domain      = "rbictg.com"
@@ -27,8 +32,28 @@ resource "aws_route53_record" "r53_record" {
   zone_id = data.aws_route53_zone.r53_zone.zone_id
 
   alias {
-    name = data.aws_ssm_parameter.ami.id
+    name = data.aws_ssm_parameter.cloud_dist.id
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
+}
+
+
+resource "aws_api_gateway_domain_name" "rest_domain" {
+  count = var.create_domain ? 1 : 0 # only create at the later stage of the pipeline
+  domain_name = "${var.short_region}-${var.stage}-${var.brand}-devportal.rbictg.com"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  # Create a domain for API Gateway
+  regional_certificate_arn = data.aws_acm_certificate.acm_cert
+}
+
+resource "aws_api_gateway_base_path_mapping" "stage_base_mapping" {
+  count = var.create_domain ? 1 : 0 # only create at the later stage of the pipeline
+  api_id      = data.aws_ssm_parameter.api_id
+  stage_name  = "prod"
+  domain_name = aws_api_gateway_domain_name.rest_domain[0].domain_name
 }
